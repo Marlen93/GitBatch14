f137ec9dd81203188198c1cd9491fd0dadbb853c
Marlen93 <marlen1993.mm@gmail.com>
2022-09-16 23:08:19 -0400

    first commit

---
+++ src/class01codes/Class01codes.java
@@ -0,0 +1,12 @@
+package class01codes;
+
+public class Class01codes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// code to launch the nuclear weapon 
+		System.out.println("Launch my favourite Tv show");
+
+	}
+
+}
