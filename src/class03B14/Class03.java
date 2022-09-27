a9e03793511edd89f84201c6b292b2030882c321
Marlen93 <marlen1993.mm@gmail.com>
2022-09-27 19:52:03 -0400

    second commit

--- src/class03B14/Class03.java
+++ src/class03B14/Class03.java
@@ -3,7 +3,9 @@ package class03B14;
 public class Class03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 
+        System.out.println("hey this is a git class");
+        System.out.println("hey this is a chang in my project");
 
 	}
 
